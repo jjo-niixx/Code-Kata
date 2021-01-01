a5a2d3e8b7808b7929e59594e3fb9f9dd43e840d
Joeun <jjo.niixxx@gmail.com>
2021-01-01 23:15:48 +0900

    Add: Codewars 문제 풀이 추가

---
+++ Codewars/CalculatingWithFunctions.js
@@ -0,0 +1,23 @@
+// 문제
+
+// For example:
+//   seven(times(five())); // must return 35
+//   four(plus(nine())); // must return 13
+//   eight(minus(three())); // must return 5
+//   six(dividedBy(two())); // must return 3
+
+function zero() {}
+function one() {}
+function two() {}
+function three() {}
+function four() {}
+function five() {}
+function six() {}
+function seven() {}
+function eight() {}
+function nine() {}
+
+function plus() {}
+function minus() {}
+function times() {}
+function dividedBy() {}
