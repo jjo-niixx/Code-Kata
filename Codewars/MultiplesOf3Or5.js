6469e110b3584bdcb9b83f9f702b335f4fcef738
Joeun <jjo.niixxx@gmail.com>
2021-01-12 14:07:18 +0900

    Modify: Codewars 풀이 리팩토링

--- Codewars/MultiplesOf3Or5.js
+++ Codewars/MultiplesOf3Or5.js
@@ -6,14 +6,8 @@
 
 function solution(number) {
   let sum = 0;
-  if (number < 0) {
-    return sum;
-  } else {
-    for (let i = 1; i < number; i++) {
-      if (i % 3 === 0 || i % 5 == 0) {
-        sum += i;
-      }
-    }
-    return sum;
+  for (let i = 1; i < number; i++) {
+    i % 3 === 0 || i % 5 == 0 ? (sum += i) : sum;
   }
+  return sum;
 }
